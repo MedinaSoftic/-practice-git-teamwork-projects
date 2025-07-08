8478eb87d6f68ba38b4ca292fa889724b560b8af
Anushap976 <mail2anusha14@gmail.com>
2025-07-08 17:00:19 -0500

    Anusha Push

--- src/main/java/com/example/classmanager/repository/StudentRepository.java
+++ src/main/java/com/example/classmanager/repository/StudentRepository.java
@@ -1,4 +1,9 @@
 package com.example.classmanager.repository;
 
-public class StudentRepository {
+import com.example.classmanager.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+    // This interface will automatically provide CRUD operations for Student entities
+    // No additional methods are needed unless custom queries are required
 }
